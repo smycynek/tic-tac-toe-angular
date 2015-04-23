b98dd10339286208582c94570d0b5c8ed4df46f1
Steven Mycynek <steven.mycynek@autodesk.com>
2015-04-23 12:19:34 -0400

    basic lint

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # tic-tac-toe-angular
-A simple tic-tac-toe game using AngularJS and a few functional programming concepts.  I'm not very familiar with DOM events in Angular, so some of the code to get a table cell, etc... is far from ideal, but I'm working on it.
+A simple tic-tac-toe game using AngularJS and a few functional and closure programming concepts.  
+I'm not very familiar with DOM events in Angular, so some of the code to get a table cell, etc... 
+is far from ideal, but I'm working on it.
 
 *  Node users, just use 'npm install' and './bin/www'
--- public/javascripts/tttGame.js
+++ public/javascripts/tttGame.js
@@ -1,11 +1,12 @@
+/*global alert */
 var tttNgAppModule = angular.module("tttNgApp", []);
-var flatten = function(arr) {
-    return arr.reduce(function(prev, curr) {
+var flatten = function (arr) {
+    return arr.reduce(function (prev, curr) {
         return prev.concat(curr);
     }, []);
 };
 
-tttNgAppModule.controller("tttCtrl", function($scope) {
+tttNgAppModule.controller("tttCtrl", function ($scope) {
 
     $scope.tableIds = [
         ["0 0", "0 1", "0 2"],
@@ -16,16 +17,16 @@ tttNgAppModule.controller("tttCtrl", function($scope) {
     $scope.player = "x";
     $scope.currentColor = "#f00";
 
-    $scope.mainRender = function() {
-      $scope.reset();
+    $scope.mainRender = function () {
+        $scope.reset();
     };
 
-    $scope.reset = function() {
+    $scope.reset = function () {
         $scope.clearStyles();
         $scope.start();
     };
 
-    $scope.start = function() {
+    $scope.start = function () {
         $scope.board = $scope.tttBoard();
         $scope.winner = undefined;
         $scope.xColor = "#f00";
@@ -34,8 +35,8 @@ tttNgAppModule.controller("tttCtrl", function($scope) {
         $scope.currentColor = $scope.xColor;
     };
 
-    $scope.togglePlayer = function() {
-        if ($scope.player == "x") {
+    $scope.togglePlayer = function () {
+        if ($scope.player === "x") {
             $scope.player = "o";
             $scope.currentColor = $scope.oColor;
         } else {
@@ -45,17 +46,17 @@ tttNgAppModule.controller("tttCtrl", function($scope) {
     };
 
 
-    $scope.clearStyles = function() {
+    $scope.clearStyles = function () {
         // alert(flatten($scope.tableIds));
-        flatten($scope.tableIds).forEach(function(id) {
+        flatten($scope.tableIds).forEach(function (id) {
             var cell = document.getElementById(id);
-            if (cell != null) {
-             cell.style.backgroundColor = "#fff";
+            if (cell !== null) {
+                cell.style.backgroundColor = "#fff";
             }
         });
     };
 
-    $scope.markCell = function(id) {
+    $scope.markCell = function (id) {
         var cell = document.getElementById(id);
 
         var vals = id.split(" ");
@@ -66,27 +67,23 @@ tttNgAppModule.controller("tttCtrl", function($scope) {
         } catch (e) {
             alert(e);
         }
-        if ($scope.winner != undefined)
+        if ($scope.winner !== undefined) {
             alert($scope.winner);
+        }
     };
 
-    $scope.tttBoard = function() {
-        //return function() {
+    $scope.tttBoard = function () {
+        //return function () {
         var m_data = [
             [undefined, undefined, undefined],
             [undefined, undefined, undefined],
             [undefined, undefined, undefined]
         ];
         var invalidArgsMessage = "Invalid arguments";
-        var dataAlreadySetMessage = "Data already set"
+        var dataAlreadySetMessage = "Data already set";
 
-        var win = function(data) {
-            var winner = undefined;
-            var tally = {
-                x: 0,
-                o: 0,
-                undefined: 0
-            };
+        var win = function (data) {
+            var winner;
             var winningTriples = [
                 [
                     [0, 0],
@@ -129,69 +126,71 @@ tttNgAppModule.controller("tttCtrl", function($scope) {
                     [0, 2]
                 ]
             ];
-            winningTriples.forEach(function(element, index, array) {
-                var allX = element.every(function(curr, idx, arr) {
+            winningTriples.forEach(function (element) {
+                var allX = element.every(function (curr) {
                     //   console.log("isx: " + curr[0] + " " + curr[1]);
-                    var theval = m_data[curr[0]][curr[1]];
+                    var theval = data[curr[0]][curr[1]];
                     // console.log("val " + theval);
                     var isX = (theval === 'x');
                     // console.log(isX);
-                    return (isX);
+                    return isX;
                 });
                 if (allX) {
                     winner = 'x';
                     return;
-                } else {
-                    var allY = element.every(function(curr, idx, arr) {
-                        var theval = m_data[curr[0]][curr[1]];
-                        var isY = (theval === 'o');
-                        //console.log(isY);
-                        return (isY);
-                    });
-                    if (allY) {
-                        winner = 'o';
-                        return;
-                    }
                 }
+                var allO = element.every(function (curr) {
+                    var theval = data[curr[0]][curr[1]];
+                    var isO = (theval === 'o');
+                    return isO;
+                });
+                if (allO) {
+                    winner = 'o';
+                    return;
+                }
+
 
             });
             return winner;
         };
-        var valid = function(r, c, val) {
-            if ((r < 0) || (r > 2) || (r === undefined) || (Number(r) == NaN))
+        var valid = function (r, c, val) {
+            if ((r < 0) || (r > 2) || (r === undefined) || (isNaN(r))) {
                 return false;
-            if ((c < 0) || (c > 2) || (c === undefined) || (Number(c) == NaN))
+            }
+            if ((c < 0) || (c > 2) || (c === undefined) || (isNaN(c))) {
                 return false;
-            if ((val !== undefined) && ((val !== 'x') && (val !== 'o')))
+            }
+            if ((val !== undefined) && ((val !== 'x') && (val !== 'o'))) {
                 return false;
+            }
             return true;
 
-        }
+        };
         return {
-            get: function(r, c) {
+            get: function (r, c) {
                 if (!valid(r, c)) {
                     throw invalidArgsMessage;
                 }
                 return m_data[r][c];
             },
-            set: function(r, c, val) {
-                if (!valid(r, c, val))
+            set: function (r, c, val) {
+                if (!valid(r, c, val)) {
                     throw invalidArgsMessage;
-                if (m_data[r][c] !== undefined)
+                }
+                if (m_data[r][c] !== undefined) {
                     throw dataAlreadySetMessage;
+                }
                 m_data[r][c] = val;
                 var winner = win(m_data);
                 return winner;
 
             },
 
-            toStr: function() {
+            toStr: function () {
                 return m_data[0][0] + "     " + m_data[0][1] + "     " + m_data[0][2] + "\n" + m_data[1][0] + "     " + m_data[1][1] + "     " + m_data[1][2] + "\n" + m_data[2][0] + "     " + m_data[2][1] + "     " + m_data[2][2];
             }
 
         };
-        //};
-    }
+    };
     $scope.board = $scope.tttBoard();
-
 });
